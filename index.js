7433acbccedbdd06b78409df488b6398ddb44374
karakulov.web.dev <karakulov.web.dev@gmail.com>
2019-03-25 05:02:57 +0700

    add https server

--- index.js
+++ index.js
@@ -4,8 +4,28 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 exports.__esModule = true;
 var express_1 = __importDefault(require("express"));
+var http_1 = __importDefault(require("http"));
+var https_1 = __importDefault(require("https"));
+var fs_1 = __importDefault(require("fs"));
 var app = express_1["default"]();
 app.use("/", express_1["default"].static(__dirname + "/public"));
-app.listen(80, function () {
-    console.log("Example app listening on port 80!");
+var httpServer = http_1["default"].createServer(app);
+httpServer.listen(80, function () {
+    console.log("HTTP Server running on port 80");
 });
+var prodaction = false;
+if (prodaction) {
+    var privateKey = void 0, certificate = void 0, ca = void 0;
+    privateKey = fs_1["default"].readFileSync("/etc/letsencrypt/live/votingpay.com/privkey.pem", "utf8");
+    certificate = fs_1["default"].readFileSync("/etc/letsencrypt/live/votingpay.com/cert.pem", "utf8");
+    ca = fs_1["default"].readFileSync("/etc/letsencrypt/live/votingpay.com/chain.pem", "utf8");
+    var credentials = {
+        key: privateKey,
+        cert: certificate,
+        ca: ca
+    };
+    var httpsServer = https_1["default"].createServer(credentials, app);
+    httpsServer.listen(443, function () {
+        console.log("HTTPS Server running on port 443");
+    });
+}
--- index.ts
+++ index.ts
@@ -1,7 +1,37 @@
 import express from "express";
+import http from "http";
+import https from "https";
+import fs from "fs";
+
 const app = express();
 
 app.use("/", express.static(__dirname + "/public"));
-app.listen(80, function() {
-  console.log("Example app listening on port 80!");
+
+const httpServer = http.createServer(app);
+httpServer.listen(80, () => {
+  console.log("HTTP Server running on port 80");
 });
+
+let prodaction = false;
+
+if (prodaction) {
+  let privateKey, certificate, ca;
+  privateKey = fs.readFileSync(
+    "/etc/letsencrypt/live/votingpay.com/privkey.pem",
+    "utf8"
+  );
+  certificate = fs.readFileSync(
+    "/etc/letsencrypt/live/votingpay.com/cert.pem",
+    "utf8"
+  );
+  ca = fs.readFileSync("/etc/letsencrypt/live/votingpay.com/chain.pem", "utf8");
+  const credentials = {
+    key: privateKey,
+    cert: certificate,
+    ca: ca
+  };
+  const httpsServer = https.createServer(credentials, app);
+  httpsServer.listen(443, () => {
+    console.log("HTTPS Server running on port 443");
+  });
+}
