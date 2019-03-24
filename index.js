f8447a0da6bddefdcb3b8f83b5b52c3e3f17c5b2
karakulov.web.dev <karakulov.web.dev@gmail.com>
2019-03-25 03:18:05 +0700

    changed port to 80

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ exports.__esModule = true;
 var express_1 = __importDefault(require("express"));
 var app = express_1["default"]();
 app.use("/", express_1["default"].static(__dirname + "/public"));
-app.listen(3000, function () {
-    console.log("Example app listening on port 3000!");
+app.listen(80, function () {
+    console.log("Example app listening on port 80!");
 });
