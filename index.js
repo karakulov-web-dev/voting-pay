15e37ca1ed39aacff60d6edce801b926aaf4e3ea
karakulov.web.dev <karakulov.web.dev@gmail.com>
2019-06-17 06:01:50 +0700

    перенос на 8081 порт

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 exports.__esModule = true;
 var express_1 = __importDefault(require("express"));
 var http_1 = __importDefault(require("http"));
-var https_1 = __importDefault(require("https"));
-var fs_1 = __importDefault(require("fs"));
 var app = express_1["default"]();
 app.use(express_1["default"].json());
 app.use("/", express_1["default"].static(__dirname + "/public"));
@@ -55,22 +53,6 @@ app.get(/./, function (req, res) {
     res.sendFile(__dirname + "/public/index.html");
 });
 var httpServer = http_1["default"].createServer(app);
-httpServer.listen(80, function () {
-    console.log("HTTP Server running on port 80");
+httpServer.listen(8081, function () {
+    console.log("HTTP Server running on port 8081");
 });
-var prodaction = true;
-if (prodaction) {
-    var privateKey = void 0, certificate = void 0, ca = void 0;
-    privateKey = fs_1["default"].readFileSync("/etc/letsencrypt/live/votingpay.com/privkey.pem", "utf8");
-    certificate = fs_1["default"].readFileSync("/etc/letsencrypt/live/votingpay.com/cert.pem", "utf8");
-    ca = fs_1["default"].readFileSync("/etc/letsencrypt/live/votingpay.com/chain.pem", "utf8");
-    var credentials = {
-        key: privateKey,
-        cert: certificate,
-        ca: ca
-    };
-    var httpsServer = https_1["default"].createServer(credentials, app);
-    httpsServer.listen(443, function () {
-        console.log("HTTPS Server running on port 443");
-    });
-}
